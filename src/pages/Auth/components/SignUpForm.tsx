89a01d83a9b08689274f791ae2d749e9bd5d8c60
Kim._.jazz <jeong011010@naver.com>
2024-11-25 00:27:22 +0900

    feat: signUpForm 추가

---
+++ src/pages/Auth/components/SignUpForm.tsx
@@ -0,0 +1,10 @@
+import React, { useState, FC, FormEvent } from "react";
+import styled from "styled-components";
+import HighlightOffOutlinedIcon from "@mui/icons-material/HighlightOffOutlined";
+import { Button, IconButton, TextField } from "@mui/material";
+
+
+const SignUpForm: FC = () => {
+    return(<></>);
+}
+export default SignUpForm;
