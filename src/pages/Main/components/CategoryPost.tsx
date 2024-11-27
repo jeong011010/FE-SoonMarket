645891335edc5d6ffce6830c1b31de9533368aec
Yusi <yusi4781@naver.com>
2024-11-28 01:51:54 +0900

    feat: main화면 카테고리 게시글 grid 적용

--- src/pages/Main/components/CategoryPost.tsx
+++ src/pages/Main/components/CategoryPost.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useGetSearchPostList from "../../../api/Post/useGetSearchPostList";
 import PostCard from "./PostCard";
 import CategoryBtnGroup from "../../../components/Post/CategoryBtnGroup";
+import styled from "styled-components";
 
 const CategoryPost: React.FC = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("전체");
@@ -14,19 +15,27 @@ const CategoryPost: React.FC = () => {
   return (
     <>
       <CategoryBtnGroup selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-      {
-        searchPostList?.posts.length ? (
-          searchPostList.posts.map((post) => (
-            <div key={post.postId}>
-              <PostCard post={post} />
-            </div>
-          ))
-        ) : (
-          <p>게시물이 없습니다.</p>
-        )
-      }
+      <PostContainer>
+        {
+          searchPostList?.posts.length ? (
+            searchPostList.posts.map((post) => (
+              <div key={post.postId}>
+                <PostCard post={post} />
+              </div>
+            ))
+          ) : (
+            <p>게시물이 없습니다.</p>
+          )
+        }
+      </PostContainer>
     </>
   )
 }
+
+const PostContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+`
 
 export default CategoryPost;
