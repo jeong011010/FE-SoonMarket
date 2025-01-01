8f5551a1598502e66c76828eabe8a7728a8ca943
Yusi <yusi4781@naver.com>
2025-01-01 21:41:52 +0900

    feat: PostPage Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import AddPostPage from "./pages/AddPost/AddPostPage";
 import { useCookies } from "react-cookie";
+import PostPage from "./pages/Post/PostPage";
 
 const theme = createTheme({
   typography: {
@@ -74,7 +75,7 @@ function App(): JSX.Element {
      { path: "/like", element: <LikePost />, private: true },
     // { path: "/mypage", element: <MyPage />, private: true },
     { path: "/main", element: <Main />, private: true },
-    // { path: "/post/:id", element: <Post />, private: true },
+    { path: "/post/:id", element: <PostPage />, private: true },
     { path: "/search", element: <Search />, private: true },
     { path: "/signup", element: <SignUp /> },
   ];
