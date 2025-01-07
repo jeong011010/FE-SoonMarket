05c1ccd6ac750dbf10b6d7845a67235a5f74841f
Kim._.jazz <jeong011010@naver.com>
2025-01-07 16:49:07 +0900

    feat: 마이페이지에서 store에 저장된 userId로 api 호출

--- src/api/Auth/useGetUserInfo.ts
+++ src/api/Auth/useGetUserInfo.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useCallback, useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 export interface UserInfo {
   id?: string;
@@ -17,6 +19,7 @@ const useGetUserInfo = () => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const [cookies] = useCookies(["access_token"]);
   const token = cookies.access_token;
+  const userId = useSelector((state: RootState) => state.auth.userId);
 
   useEffect(() => {
     if (!token) {
@@ -32,7 +35,7 @@ const useGetUserInfo = () => {
     }
 
     try {
-      const response = await axios.get<UserInfo>(`${apiUrl}/users/1`);
+      const response = await axios.get<UserInfo>(`${apiUrl}/users/${userId}`);
       setUserInfo(response.data);
       console.log("User information fetched successfully.");
     } catch (error) {
