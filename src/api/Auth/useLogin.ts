496bc0fbd0a074b70762e01f6f7ea929b58624e4
Kim._.jazz <jeong011010@naver.com>
2025-01-07 16:45:47 +0900

    feat: 로그인시 토큰값 디코딩하여 데이터 store에 저장

--- src/api/Auth/useLogin.ts
+++ src/api/Auth/useLogin.ts
@@ -2,7 +2,28 @@ import axios from "axios";
 import { Cookies } from "react-cookie";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setIsAuthenticated } from "../../redux/modules/auth";
+import { setIsAuthenticated, setRole, setUserId } from "../../redux/modules/auth";
+import { jwtDecode } from "jwt-decode";
+
+interface JwtPayload {
+	sub?: string; // userId
+	auth?: string; // role
+	[key: string]: any;
+}
+
+function extractTokenData(token: string): {userId: string; role:string;} | null {
+  try {
+    const decoded = jwtDecode<JwtPayload>(token);
+		console.log(decoded);
+		return {
+      userId: decoded.sub || "",
+      role: decoded.auth || "",
+    };
+  } catch (error) {
+    console.error("Failed to decode token:", error);
+    return null; // 디코딩 실패 시 null 반환
+  }
+}
 
 const useLogin = () => {
 	const apiUrl = import.meta.env.VITE_API_URL as string; // 환경 변수가 존재한다고 가정
@@ -22,7 +43,15 @@ const useLogin = () => {
 				secure: false,
 				sameSite: "strict",
 			})
-			dispatch(setIsAuthenticated(true));
+			const tokenData = extractTokenData(token);
+        if (tokenData) {
+          const { userId, role } = tokenData;
+
+          // Redux 상태 업데이트
+          dispatch(setIsAuthenticated(true));
+          dispatch(setUserId(userId));
+          dispatch(setRole(role));
+        }
 			navigate("/main");
 		}).catch(error => {
 			dispatch(setIsAuthenticated(false));
