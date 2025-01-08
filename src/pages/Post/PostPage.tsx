68c0822bf0805837b95924ab05b6618fc1278f71
Yusi <yusi4781@naver.com>
2025-01-08 18:49:38 +0900

    refactor: 게시글 상세 페이지 게시글 올린 사람 컴포넌트 분리

--- src/pages/Post/PostPage.tsx
+++ src/pages/Post/PostPage.tsx
@@ -1,12 +1,11 @@
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { Button, IconButton } from "@mui/material";
-import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useEffect } from "react";
 import useGetPost from "../../api/Post/useGetPost";
 import PostImgBox from "./components/PostImgBox";
+import PostMaster from "./components/PostMaster";
 
 const PostPage = () => {
   const navigate = useNavigate();
@@ -30,21 +29,7 @@ const PostPage = () => {
       {
         post?.images && <PostImgBox images={post.images} />
       }
-      <UserBox>
-        <ProfileImg />
-        <ProfileText>
-          <p style={{ margin: 1 }}>이름</p>
-          <p style={{ margin: 1 }}>신고 횟수 0</p>
-        </ProfileText>
-        <BtnBox>
-          <IconButton>
-            <FavoriteBorderIcon fontSize="large" />
-          </IconButton>
-          <IconButton>
-            <ReportGmailerrorredIcon fontSize="large" />
-          </IconButton>
-        </BtnBox>
-      </UserBox>
+      <PostMaster />
       <ContentBox>
         <TitleBox>
           <Title>
@@ -74,35 +59,6 @@ const PostContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding-bottom: 60px;
-`;
-
-const UserBox = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ProfileImg = styled.div`
-  width: 70px;
-  height: 70px;
-  background: gray;
-  margin: 10px;
-`;
-
-const ProfileText = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin: 10px 0px;
-`;
-
-const BtnBox = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0px 10px 0px auto;
 `;
 
 const ContentBox = styled.div`
