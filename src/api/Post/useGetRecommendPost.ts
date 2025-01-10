eb197618c98186e11fcf9d7700636199bf9a2edd
Kim._.jazz <jeong011010@naver.com>
2025-01-10 16:12:25 +0900

    fix: 추천 페이지 api 토큰값 추가 수정 완료

--- src/api/Post/useGetRecommendPost.ts
+++ src/api/Post/useGetRecommendPost.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useCallback, useState } from "react"
+import { useCookies } from "react-cookie";
 
 
 type PostImage = {
@@ -21,14 +22,31 @@ type Post = {
 };
 
 const useGetRecommendPost = () => {
-  const [recommendPosts, setRecommendPosts] = useState<Post[]>([])
+  const [recommendPosts, setRecommendPosts] = useState<Post[]>([]);
+  const [cookies] = useCookies(["access_token"]);
+  const token = cookies.access_token;
 
   const getRecommendPosts = useCallback(async () => {
-    const apiUrl = import.meta.env.VITE_API_URL;
-    await axios.get(`${apiUrl}/posts/random`)
-    .then(response => setRecommendPosts(response.data))
-    .catch(error => console.error(error));
-  }, [])
+    const apiUrl = import.meta.env.VITE_API_URL as string;
+    if (!token) {
+      console.error("Cannot fetch posts without a valid token.");
+      return;
+    }
+
+    try {
+      const response = await axios.get<Post[]>(`${apiUrl}/posts/random`, {
+        headers: {
+          Authorization: `${token}`,
+        },
+      });
+      console.log(response.data);
+      setRecommendPosts(response.data);
+      console.log("My posts fetched successfully.");
+    } catch (error) {
+      console.error("Error fetching my posts:", error);
+    }
+  }, [token]);
+  
 
   return { getRecommendPosts, recommendPosts }
 }
