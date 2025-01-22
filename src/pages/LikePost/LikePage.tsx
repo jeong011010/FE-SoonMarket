ff0d51326497a270c56c037ffe12927ce6cf926a
Kim._.jazz <jeong011010@naver.com>
2025-01-22 22:24:00 +0900

    fix: 관심목록 TopBar 삭제

--- src/pages/LikePost/LikePage.tsx
+++ src/pages/LikePost/LikePage.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import TopBar from "./components/TopBar";
 import { useEffect } from "react";
 import useGetLikePost from "../../api/Post/useGetLikePost";
 import WidePostCard from "../../components/Post/WidePostCard";
@@ -14,7 +13,6 @@ const LikePage = () => {
 
   return (
     <LikeContainer>
-      <TopBar />
       <h1 style={{ margin: "0px 0px 0px 20px", alignSelf: "flex-start" }}>관심 목록</h1>
       {likePosts && likePosts.length > 0 ? (
         likePosts.map((data) => (
