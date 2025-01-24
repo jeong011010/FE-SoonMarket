759c5fad816e709d6efc8df0a3843e10095a8c79
Yusi <yusi4781@naver.com>
2025-01-25 05:37:23 +0900

    feat: 게시글 추가 함수에 axios intercept 적용

--- src/api/Post/useAddPost.ts
+++ src/api/Post/useAddPost.ts
@@ -1,21 +1,13 @@
-import axios from "axios";
-import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import axiosInstace from "../axiosInstance";
 
 const useAddPost = () => {
-  const apiUrl = import.meta.env.VITE_API_URL;
-  const navigate = useNavigate()
-  const [cookies] = useCookies(['access_token']);
-  const token = cookies.access_token;
+  const navigate = useNavigate();
 
   const addPost = async (formData: FormData) => {
     try {
       console.log('게시물 등록 시도', formData);
-      await axios.post(`${apiUrl}/posts`, formData, {
-        headers: {
-          Authorization: `${token}`
-        }
-      });
+      await axiosInstace.post("/posts", formData);
       console.log('게시물 추가 완료');
       navigate('/main');
     } catch (error) {
