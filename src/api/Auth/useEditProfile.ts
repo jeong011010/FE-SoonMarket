772e1ada7739516eb8a2ceae675bc4859fe7e8dc
Yusi <yusi4781@naver.com>
2025-02-01 01:38:44 +0900

    refactor: 프로필 수정 axios istance를 활용하는 방식으로 수정

--- src/api/Auth/useEditProfile.ts
+++ src/api/Auth/useEditProfile.ts
@@ -1,22 +1,11 @@
-import axios from "axios";
-import { useCookies } from "react-cookie";
+import axiosInstance from "../axiosInstance";
 
 const useEditProfile = () => {
-  const apiUrl = import.meta.env.VITE_API_URL;
-  const [cookies] = useCookies(["access_token"]);
-  const token = cookies.access_token;
 
   const editProfile = async (formData: FormData) => {
     try {
       console.log("프로필 수정 시도", formData);
-
-      // PUT 요청
-      await axios.put(`${apiUrl}/users/me`, formData, {
-        headers: {
-          Authorization: `${token}`, // 토큰 포함
-        },
-      });
-
+      await axiosInstance.put(`/users/me`, formData);
       console.log("프로필 수정 성공");
     } catch (error) {
       console.error("프로필 수정 시도 중 오류 발생", error);
