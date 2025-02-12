3d327440ce732d27101dc73da56c19c2060e7506
Kim._.jazz <jeong011010@naver.com>
2025-02-12 20:09:26 +0900

    fix: 채팅방에서 판매자, 구매자 두명의 데이터를 동시에 불러오기 위해 getUserInfo 데이터타입 수정

--- src/api/Auth/useGetUserInfo.ts
+++ src/api/Auth/useGetUserInfo.ts
@@ -4,11 +4,18 @@ import axiosInstance from "../axiosInstance";
 
 const useGetUserInfo = () => {
   const [userInfo, setUserInfo] = useState<User | null>(null);
-  const getUserInfo = useCallback(async (userId: string | number) => {
-    await axiosInstance.get<User>(`/users/${userId}`)
-      .then(response => setUserInfo(response.data))
-      .catch(error => console.error("유저 정보 불러오기 중 오류 발생", error));
+
+  const getUserInfo = useCallback(async (userId: string | number): Promise<User> => {
+    try {
+      const response = await axiosInstance.get<User>(`/users/${userId}`);
+      setUserInfo(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("유저 정보 불러오기 중 오류 발생", error);
+      throw error;
+    }
   }, []);
+
 
   return { userInfo, getUserInfo };
 };
