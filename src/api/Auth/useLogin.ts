2f0691fa2d34639cd64478c4cda4d55ab15b3e8e
Yusi <yusi4781@naver.com>
2025-03-02 16:07:09 +0900

    feat: login에 fcmToken 추가

--- src/api/Auth/useLogin.ts
+++ src/api/Auth/useLogin.ts
@@ -3,6 +3,7 @@ import { Cookies } from "react-cookie";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setIsAuthenticated } from "../../redux/modules/auth";
+import { getFCMToken } from "../../firebaseConfig";  // 🔹 FCM 토큰 가져오는 함수 추가
 
 const useLogin = () => {
 	const apiUrl = import.meta.env.VITE_API_URL as string;
@@ -11,32 +12,35 @@ const useLogin = () => {
 	const dispatch = useDispatch();
 
 	const login = async (email: string, password: string): Promise<void> => {
+		const fcmToken = await getFCMToken();  // 🔹 FCM 토큰 가져오기
 		await axios.post(`${apiUrl}/users/login`, {
 			email,
 			password,
-		}).then(response => {
+			fcmToken,
+		}).then((response) => {
 			const accessToken = response.data.accessToken;
 			const refreshToken = response.data.refreshToken;
+
 			cookies.set("access_token", accessToken, {
 				path: "/",
 				httpOnly: false,
 				secure: false,
 				sameSite: "strict",
-			})
+			});
 			cookies.set("refresh_token", refreshToken, {
 				path: "/",
 				httpOnly: false,
 				secure: false,
 				sameSite: "strict",
-			})
+			});
+
 			dispatch(setIsAuthenticated(true));
 			navigate("/main");
-		}).catch(error => {
+		}).catch((error) => {
 			dispatch(setIsAuthenticated(false));
-			throw new Error(error);
-		})
+			console.error("로그인 실패:", error);
+		});
 	};
-
 	return login;
 };
 
