03d2c1f680e8b6e08351257a804d3ae6fcbe1f1a
Yusi <yusi4781@naver.com>
2025-03-03 15:48:16 +0900

    feat: 알림 팝업 구현

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -1,32 +1,69 @@
-import { Badge, IconButton } from "@mui/material"
+import { Badge, Divider, IconButton, Menu, MenuItem } from "@mui/material"
 import styled from "styled-components";
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import SearchIcon from '@mui/icons-material/Search';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useGetNotificationList from "../../api/Notification/useGetNotificationList";
 
 const TopBar: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const navigate = useNavigate();
+	const [searchQuery, setSearchQuery] = useState("");
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const { notificationList, getNotificationList } = useGetNotificationList();
+	const navigate = useNavigate();
 
-  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    navigate(`/search?query=${searchQuery}`);
-  }
+	useEffect(() => {
+		getNotificationList();
+	}, [getNotificationList]);
 
-  return (
-    <TopBarContainer>
-      <SearchBarContainer onSubmit={handleSearch}>
-        <Input type="text" placeholder="검색어 입력" onChange={(e) => setSearchQuery(e.target.value)} />
-        <IconButton type="submit">
-          <SearchIcon />
-        </IconButton>
-      </SearchBarContainer>
-      <AlarmBadge color="secondary" variant="dot" sx={{ width: "30px", height: "30px" }}>
-        <NotificationsNoneOutlinedIcon sx={{ width: "30px", height: "30px" }} />
-      </AlarmBadge>
-    </TopBarContainer>
-  )
+	console.log(notificationList);
+
+	const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		navigate(`/search?query=${searchQuery}`);
+	}
+
+	const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	}
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	}
+
+	return (
+		<TopBarContainer>
+			<SearchBarContainer onSubmit={handleSearch}>
+				<Input type="text" placeholder="검색어 입력" onChange={(e) => setSearchQuery(e.target.value)} />
+				<IconButton type="submit">
+					<SearchIcon />
+				</IconButton>
+			</SearchBarContainer>
+			<IconButton onClick={handleOpen}>
+				<AlarmBadge color="secondary" variant="dot" sx={{ width: "30px", height: "30px" }}>
+					<NotificationsNoneOutlinedIcon sx={{ width: "30px", height: "30px" }} />
+				</AlarmBadge>
+			</IconButton>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				onClick={handleClose}
+			>
+				{
+					notificationList.map((notification) => (
+						<>
+							<MenuItem key={notification.id}>
+								{notification.message}
+							</MenuItem>
+							<Divider />
+						</>
+					))
+				}
+			</Menu>
+		</TopBarContainer>
+	)
 }
 
 const SearchBarContainer = styled.form`
