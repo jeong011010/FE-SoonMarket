ee52cb778327991968a954b2d31408e1f457a92c
Yusi <yusi4781@naver.com>
2025-03-03 16:34:33 +0900

    fix: fcm 설정 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'generate-firebase-env',
-      buildEnd() {
-        const firebaseEnv = `
-          self.FIREBASE_CONFIG = {
-            apiKey: "${process.env.VITE_FIREBASE_API_KEY}",
-            authDomain: "${process.env.VITE_FIREBASE_AUTH_DOMAIN}",
-            projectId: "${process.env.VITE_FIREBASE_PROJECT_ID}",
-            storageBucket: "${process.env.VITE_FIREBASE_STORAGE_BUCKET}",
-            messagingSenderId: "${process.env.VITE_FIREBASE_MESSAGING_SENDER_ID}",
-            appId: "${process.env.VITE_FIREBASE_APP_ID}"
-          };
-        `;
-        fs.writeFileSync('public/firebase-env.js', firebaseEnv);
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  function generateFirebaseEnv() {
+    const firebaseEnv = `
+      self.FIREBASE_CONFIG = {
+        apiKey: "${env.VITE_FIREBASE_API_KEY}",
+        authDomain: "${env.VITE_FIREBASE_AUTH_DOMAIN}",
+        projectId: "${env.VITE_FIREBASE_PROJECT_ID}",
+        storageBucket: "${env.VITE_FIREBASE_STORAGE_BUCKET}",
+        messagingSenderId: "${env.VITE_FIREBASE_MESSAGING_SENDER_ID}",
+        appId: "${env.VITE_FIREBASE_APP_ID}"
+      };
+    `;
+    fs.writeFileSync(path.resolve('public/firebase-env.js'), firebaseEnv);
+  }
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'generate-firebase-env',
+        configureServer() {
+          generateFirebaseEnv(); // 개발 서버에서 실행
+        },
+        buildEnd() {
+          generateFirebaseEnv(); // 빌드 시 실행
+        }
       }
-    }
-  ],
-  base: "/",
-  define: {
-    global: "globalThis",
-  },
-})
+    ],
+    base: "/",
+    define: {
+      global: "globalThis",
+    },
+  };
+});
