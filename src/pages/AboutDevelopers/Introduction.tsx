a9a098e4a4a5747c4da3580751a3dc1a0de1ff38
Junse0lee <ejrqlddl1@sch.ac.kr>
2025-03-08 01:36:19 +0900

    fix: 동아리 소개 삭제

--- src/pages/AboutDevelopers/Introduction.tsx
+++ src/pages/AboutDevelopers/Introduction.tsx
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import { IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
-import SlideFunction from "./SlidingFunction"
+import AboutDevelopers from "./components/AboutDevelopers";
 
-const Introduction: React.FC =() => {
-    const navigate= useNavigate();
+const Introduction: React.FC = () => {
+    const navigate = useNavigate();
 
     return (
         <IntroductionContainer>
@@ -20,18 +20,18 @@ const Introduction: React.FC =() => {
                 <UnderLine />
             </Header>
             <FormWrapper>
-                <SlideFunction/>
+                <AboutDevelopers />
             </FormWrapper>
         </IntroductionContainer>
     );
 };
-//Styled Components
+
+// Styled Components
 const IntroductionContainer = styled.div`
-    dispaly: flex;
+    background-color: rgb(76, 109, 142); /* 파란색 배경 */
+    height: 100vh; /* 화면 전체 높이 */
+    display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-family: 'SUIT', sans-serif;
 `;
 
 const Header = styled.div`
@@ -40,6 +40,8 @@ const Header = styled.div`
     flex-direction: column;
     align-items: center;
     position: relative;
+    margin-bottom: 0px;
+    background-color: white;
 `;
 
 const BackButton = styled.div`
@@ -49,28 +51,24 @@ const BackButton = styled.div`
 `;
 
 const Title = styled.h1`
-  margin: 100px; /* 원하는 margin 값으로 설정 */
-  margin-bottom: 55px;
+  margin-top: 80px; /* 원하는 margin 값으로 설정 */
+  margin-bottom: 40px;
   font-family: 'SUIT', sans-serif; /* 폰트 설정 */
   font-size: 37px;
-  `;
+`;
 
-const UnderLine = styled.div `
-    width: 100%;
+const UnderLine = styled.div`
+  width: 100%;
   height: 5px; /* 선의 두께 */
   background-color: #D9E9F9;
-  margin-bottom: 3px;
+  margin-bottom: 0px;
 `;
 
 const FormWrapper = styled.div`
-  width: 100%;
-  max-width: 390px; 
-  height: calc(100% - 160px);
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  background: white;
+    flex: 1; /* 남은 공간을 차지하도록 설정 */
+    display: flex;
+    justify-content: center; /* 수직 가운데 정렬 */
+    align-items: center; /* 수평 가운데 정렬 */
 `;
 
 export default Introduction;
