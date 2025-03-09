99565b08dbab00602415fbbdd71d20d560f69476
Kim._.jazz <jeong011010@naver.com>
2025-03-09 15:53:11 +0900

    fix: UserInfo return 타입 수정

--- src/api/Auth/useGetUserInfo.ts
+++ src/api/Auth/useGetUserInfo.ts
@@ -5,11 +5,12 @@ import axiosInstance from "../axiosInstance";
 const useGetUserInfo = () => {
   const [userInfo, setUserInfo] = useState<User | null>(null);
 
-  const getUserInfo = useCallback(async (userId: string | number) => {
-    if (userId) {
+  const getUserInfo = useCallback(async (userId: string | number): Promise<User> => {
+    {
       try {
         const response = await axiosInstance.get<User>(`/users/${userId}`);
         setUserInfo(response.data);
+        return response.data; // ✅ 반환 추가
       } catch (error) {
         console.error("유저 정보 불러오기 중 오류 발생", error);
         throw error;
