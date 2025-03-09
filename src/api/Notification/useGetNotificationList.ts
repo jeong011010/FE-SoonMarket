84a192db4fa104587e2e554fa84769afaf8d074e
Yusi <yusi4781@naver.com>
2025-03-09 16:08:52 +0900

    fix: 알림 리스트가 시간 역순으로 저장되는 버그 수정

--- src/api/Notification/useGetNotificationList.ts
+++ src/api/Notification/useGetNotificationList.ts
@@ -8,7 +8,7 @@ const useGetNotificationList = () => {
 	const getNotificationList = useCallback(async () => {
 		await axiosInstance.get("/notification")
 			.then((response) => {
-				setNotificationList(response.data.reverse());
+				setNotificationList(response.data);
 			})
 			.catch((error) => {
 				console.error("알림 목록 가져오기 오류", error);
