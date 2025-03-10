267fcdfebfaa8d8f15a2f79412c9c9e0de4aa7df
Yusi <yusi4781@naver.com>
2025-03-10 22:45:33 +0900

    style: 알림 margin 수정

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -113,7 +113,7 @@ const TopBar: React.FC = () => {
 								$read={notification.read}
 								onClick={() => handleNotificationClick(notification.id, notification.postId, notification.message, notification.read)}
 							>
-								{postImgList[idx] && <img src={postImgList[idx].imageUrl} alt="미리보기" width={60} height={60} style={{ margin: "15px 10px 15px 10px" }} />}
+								{postImgList[idx] && <img src={postImgList[idx].imageUrl} alt="미리보기" width={60} height={60} style={{ margin: "10px 10px 10px 10px" }} />}
 								<ItemContent>
 									{notification.message}
 									<p style={{ margin: 0 }}>{getTimeAgo(new Date(notification.createdAt))}</p>
@@ -217,6 +217,7 @@ const NotificationItem = styled(MenuItem) <{ $read: boolean }>`
     padding: 10px 0px 0px 10px;
     align-items: flex-start;
     background-color: ${(props) => (props.$read ? "#f0f0f0" : "white")}; /* 읽은 알림이면 회색, 안 읽었으면 흰색 */
+		width: 103%;
 
     &:hover {
       background-color: #e0e0e0;
@@ -225,7 +226,7 @@ const NotificationItem = styled(MenuItem) <{ $read: boolean }>`
 `;
 
 const ItemContent = styled.div`
-	margin: 23px 0px 0px 5px;
+	margin: 17px 0px 0px 5px;
 `
 
 
